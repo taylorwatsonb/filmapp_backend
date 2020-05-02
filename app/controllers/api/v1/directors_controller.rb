3849fac7de66b3c80ff7812f9a80bf0005e50d87
github taylorwatsonb <github taylorwatsonb@gmail.com>
2020-05-02 18:33:59 -0400

    directors controller index and create routes

--- app/controllers/api/v1/directors_controller.rb
+++ app/controllers/api/v1/directors_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::DirectorsController < ApplicationController
+    def index 
+        directors = Director.all 
+        render json: directors
+    end
+
+    def create 
+        director = Director.new(director_params)
+        if director.save
+         render json: director, status: :accepted
+        else
+        render json: {errors: director.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def director_params
+        params.require(:director).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :directors, only: [:index]
+      resources :directors, only: [:index, :create]
   end
 end 
 
