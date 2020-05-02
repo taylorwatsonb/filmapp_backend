9df4e8dbe342bd902ca943527701fc1b880f60ab
github taylorwatsonb <github taylorwatsonb@gmail.com>
2020-05-02 17:51:14 -0400

    director routes and generated controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Director.destroy_all
+Movie.destroy_all
 
 ava = Director.create(name: "Ava DuVernay")
 spike = Director.create(name: "Spike Lee")
